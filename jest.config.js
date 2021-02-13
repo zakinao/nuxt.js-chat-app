0f3c1ce1cb9c858f806b1c6d7d2df372bf1770af
zakinao <zakinaororo@gmail.com>
2021-02-13 11:57:37 +0900

    add test

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,6 @@
 module.exports = {
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
-  },
-  moduleFileExtensions: [
-    'js',
-    'vue',
-    'json'
-  ],
+  moduleFileExtensions: ['js'],
   transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+   '^.+\\.js$': 'babel-jest',
   },
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
-  ]
-}
+ }
